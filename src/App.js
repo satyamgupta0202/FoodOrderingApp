f9898bfbe3f8e649b7e1248a5af617df8ed9eb75
Satyam Gupta <2019335@iiitdmj.ac.in>
2021-07-22 12:19:55 +0000

    Meals Summary Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/header";
 import React ,{Fragment} from 'react'
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header/>
+    <Meals/>
     </Fragment>
   );
 }
